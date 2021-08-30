be2d81d382551aca187d14f5729ad21d7aaa58f1
CSI-Mario-Sanchez <22-076@sanignacio.pr>
2021-08-30 09:09:08 -0400

    CSI-Mario-Sanchez
    Print changed to one line with \n line breaks and an additional file was added to import the print method into another file.

---
+++ Projects/ASCII_Initials.py
@@ -0,0 +1,23 @@
+def print_A():
+    print("    A    \n   / \   \n  /___\  \n /     \ \n/       \\")
+def print_I():
+    print(" _______ \n    |    \n    |    \n    |    \n ___|___ ")
+def print_M():
+    print("|\     /|\n| \   / |\n|  \ /  |\n|   V   |\n|       |")
+def print_O():
+    print("  _____  \n /     \ \n |     | \n |     | \n \_____/ ")
+def print_R():
+    print("  ____  \n  |   \ \n  |___/  \n  | \   \n  |  \  \n  |   \  ")
+def print_name():
+    print_M()
+    print()
+    print_A()
+    print()
+    print_R()
+    print()
+    print_I()
+    print()
+    print_O()
+    print()
+print_name()
+test = input();
---
+++ Projects/ASCII_Name_Print.py
@@ -0,0 +1,5 @@
+import sys
+import ASCII_Initials
+
+print_name()
+test = input();
