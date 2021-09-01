26335aed88231ed7d1e981c0c725d20e13aeb7e6
CSI-Ignacio-Guzman <89158849+CSI-Ignacio-Guzman@users.noreply.github.com>
2021-09-01 14:53:51 -0400

    DOS O TRES

---
+++ Projects/ASCII Initials.py
@@ -0,0 +1,8 @@
+print("""         
+IIIII      A           GGGGGGGGGGGGGG      L
+  I      A   A         GG              G   L
+  I     AAAAAAA       GGG                  L
+  I    A       A      GGG                  L
+  I    A        A     GGGGGGGGGGGGGGGG     LLLLLLLLLL
+IIIII
+""")
