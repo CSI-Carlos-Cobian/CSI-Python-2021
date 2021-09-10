9c27c532174bac38e9c7eab57a7be24c93723d0a
CSI-Ignacio-Guzman <89158849+CSI-Ignacio-Guzman@users.noreply.github.com>
2021-09-10 14:33:01 -0400

    Create Module4.py

---
+++ Modules/Module4/Module4.py
@@ -0,0 +1,8 @@
+print("What did you have for breakfest?")
+meal = "waffles"
+print(meal)
+print("What was your lunch?")
+meal = "Chicken Tenders"
+print("What was your dinner?")
+meal = "Hamburger with fries"
+print(meal)
