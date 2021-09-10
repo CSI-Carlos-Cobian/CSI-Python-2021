6d00930fa7c8b43b67a686c256a4a9ed0e93e46e
CSI-Daniel-Cintron <89158899+CSI-Daniel-Cintron@users.noreply.github.com>
2021-09-10 14:38:37 -0400

    module4.py

---
+++ Modules/Module4/Modulo4.py
@@ -0,0 +1,9 @@
+print("What did you have for Breakfast?")
+meal="sandwich"
+print(meal)
+print("What did you have for lunch?")
+meal="pasta"
+print(meal)
+print("What did you have for dinner?")
+meal="lasagna"
+print(meal)
