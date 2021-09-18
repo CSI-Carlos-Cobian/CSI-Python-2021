503992de0b8749f16b73b2dea0771585d0e1d16d
CSI-Carlos-Cobian <carlos.cobian@sanignacio.pr>
2021-09-18 18:36:59 -0400

    no message

---
+++ Modules/Module5/Module5.py
@@ -0,0 +1,14 @@
+x = 45
+
+if(x == 0):
+    print("The value of x is 0")
+elif(x > 50):
+  print("The value of x is more than 50")
+else:
+    print("The value of x is lower than 50, and is not 0")
+
+import random
+
+foo = ['firstValue', 'secondValue', 'anotherValue']
+computerChoice = random.choice(foo)
+print(computerChoice)
