acda49944559d34cf88948fb4c444d0e277d9f82
CSI-Daniel-Cintron <89158899+CSI-Daniel-Cintron@users.noreply.github.com>
2021-09-20 15:10:53 -0400

    Calculator

---
+++ Modules/Module4/Daniel Cintron-Calculator.py
@@ -0,0 +1,27 @@
+# print("Hello World")
+# print(input("What is your name?: "))
+# myName = input ()
+# print("It is good to mmet you", myName)
+# print("What is your age?  :") 
+# myAge = input()
+# print("You will be " + str(int(myAge)+5)+" in 5 years")
+print("Rectangle Area Calculator")
+print("""
+-----------------------
+|                     |
+|                     |
+|                     |
+|                     |
+|                     | 
+-----------------------
+""")
+mywidth = int(input("What is the base or width of the square: "))
+myheight = int(input("What is the height of the figure: "))
+print("The Area of the rectangle is: ", mywidth * myheight)
+print()
+def AreaOfRectangle(width, height):
+ print(f"Area of the rectangle is {int(width) * int(height)}")  
+print("Width is 15 cm")
+print("Height is 11 cm")
+AreaOfRectangle(15, 11) 
+
