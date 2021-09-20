5eb466fbc8bbd229d9179821351357e2f0a5c957
CSI-Ignacio-Guzman <89158849+CSI-Ignacio-Guzman@users.noreply.github.com>
2021-09-20 14:01:49 -0400

    Update Module4.py

--- Modules/Module4/Module4.py
+++ Modules/Module4/Module4.py
@@ -1,8 +1,11 @@
-print("What did you have for breakfest?")
-meal = "waffles"
-print(meal)
-print("What was your lunch?")
-meal = "Chicken Tenders"
-print("What was your dinner?")
-meal = "Hamburger with fries"
-print(meal)
+# print("What did you have for breakfest?")
+# meal = "waffles"
+# print(meal)
+# print("What was your lunch?")
+# meal = "Chicken Tenders"
+# print("What was your dinner?")
+# meal = "Hamburger with fries"
+# print(meal)
+def greetstudent(name):
+    print(f"Hello{name}"!")
+greetstudent(Ignacio Guzman)
