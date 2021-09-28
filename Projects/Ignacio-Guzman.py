576ed1c1d3cb56f6f04d4f8316ed14405c74a382
CSI-Ignacio-Guzman <89158849+CSI-Ignacio-Guzman@users.noreply.github.com>
2021-09-28 14:55:15 -0400

    space
    dos o tres space

---
+++ Projects/Ignacio-Guzman.py
@@ -0,0 +1,21 @@
+planets = ("Mercury", "Venus", "Earth","Mars", "Jupiter","Saturn","Uranus" , "Neptune")
+g_ms2   = [3.7, 8.87 ,9.81, 3.711, 24.79, 10.44 , 8.69, 11.5]
+
+print("Calculate your weight in a diffrent planet")
+
+myWeigh = int(input("What is your weight (pounds)?")
+myplanet = input(f"Select a planet from the list:   {Planets}")
+
+def calculateWeight(planet, mass ) :
+        print(f"/nEarth mass in pounds is : {mass})
+
+w_kg= mass / 2.2046
+ print(f"Mass in kg : {w_kg}")
+
+n_lb = 4.45 
+
+planet_index = planet.index(planet)
+print(f"Weight in {planets[planet_index]}mmis {w_kg * g_ms[planet_index])  / n_lb} lb")
+
+
+calculateWeight(myPlanet , myWeigh)
