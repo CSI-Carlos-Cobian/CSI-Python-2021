194a2c6766466bdea73ac8c1a3897d17de96e8ea
CSI-Jose-Quintana <jose.quintana@sanignacio.pr>
2021-09-30 15:25:34 -0400

    Cambios

--- Projects/Your weight in space/Jose-Quintana-Space.py
+++ Projects/Your weight in space/Jose-Quintana-Space.py
@@ -1,21 +1,19 @@
 planets = ["Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune"]
 g_ms2 = [3.7, 8.87, 9.81, 3.711, 24.79, 10.44, 8.69, 11.15]
 
-print("Calculate your weight in a different planet")
-
+print("Calculate your weight in a different planet of our Solar System")
 myWeight = int(input("What is your weight (pounds)? "))
-myPlanet = input(f"Select a planet from the list:  {planets}")
 
+myPlanet = input(f"Select a planet from the list: {planets}\n")
 def calculateWeight(planet, mass):
-    print(f"\nEarth mass in pounds is: {mass}")
+    print(f"Earth mass in pounds is: {mass}")
 
     w_kg = mass / 2.2046
     print(f"Mass in kg: {w_kg}")
 
-    n_lb = 4.45 
+    n_lb = 4.45
 
     planet_index = planets.index(planet)
-    print(f"Weight in {planets[planet_index]} is {(w_kg * g_ms2[planet_index]) / n_lb} lb")
-
+    print(f"Your weight in {planets[planet_index]} is {(w_kg * g_ms2[planet_index]) / n_lb} lb. ")
 
 calculateWeight(myPlanet, myWeight)
