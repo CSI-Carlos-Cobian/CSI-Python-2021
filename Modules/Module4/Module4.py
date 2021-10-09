43e2716d9f4851207789ab980a63379e022264df
Yusa Yuichi <89141578+Yusayuichiwastaken@users.noreply.github.com>
2021-10-09 11:14:52 -0400

    Module4 Final

--- Modules/Module4/Module4.py
+++ Modules/Module4/Module4.py
@@ -1,4 +1,11 @@
 def greetStudent(name):
   print(f"Hello {name}!")
 name = "Javier Estremera, how you doing" 
-greetStudent(name)
+greetStudent(name) 
+
+name = input("What did you have for Breakfast? ")
+studentId = input("What was your lunch? ")
+dinner = input("Have you had dinner?")
+print(f"Hello {name}! ")
+print(f"Your Student id is: {studentId} ")
+print(f"Currently, the time is: {dinner} ") 
