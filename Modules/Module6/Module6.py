ec84ce291e342aaec239b7c6b9475a9b9103ae5d
Yusayuichiwastaken <yusayuichiwastaken@gmail.com>
2021-10-13 21:23:26 -0400

    Mars Py

---
+++ Modules/Module6/Module6.py
@@ -0,0 +1,12 @@
+weigth = input("Whats your weights in pounds")  
+Choose = input(" Select your planet Mars, Venus, Mercury, Earth, Jupiter, Saturn, Uranus, Neptune") 
+
+
+  
+Venus = ( "weigth"/8.87) 
+Earth = ("weigth" / 9.81) 
+mars = ( "weigth"/3.711)  
+Jupiter = ("weigth"/24.79) 
+Saturn = ("weigth"/10.44) 
+Uranus = ("weigth"/8.69) 
+Neptune = ("weigth"/11.15)
