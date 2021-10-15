87e1221946492c192012bdf1fff9b1d9634b1344
CSI-Carlos-Cobian <carlos.cobian@sanignacio.pr>
2021-10-15 18:48:34 -0400

    JQ comments on space function

--- Projects/Your weight in space/Jose-Quintana-Space.py
+++ Projects/Your weight in space/Jose-Quintana-Space.py
@@ -1,19 +1,35 @@
+# Planets.py
+# Calculate your weight in another planet!
+
 planets = ["Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune"]
 g_ms2 = [3.7, 8.87, 9.81, 3.711, 24.79, 10.44, 8.69, 11.15]
 
-print("Calculate your weight in a different planet of our Solar System")
-myWeight = int(input("What is your weight (pounds)? "))
+print("Calculate your weight in a different planet of our Solar System!")
 
-myPlanet = input(f"Select a planet from the list: {planets}\n")
-def calculateWeight(planet, mass):
-    print(f"Earth mass in pounds is: {mass}")
+# Query the user for a integer mass. Any other data type will result in an error.
+myMass = int(input("What is your mass (pounds)? "))
 
-    w_kg = mass / 2.2046
-    print(f"Mass in kg: {w_kg}")
+# Convert the planets list into a string. Remove the special characters: ' [ ] 
+planetsString = str(planets).replace('\'','').replace('[','').replace(']','')
+myPlanet = input(f"Select a planet from the list: {planetsString} ")
 
-    n_lb = 4.45
+def calculateWeight(planet:str,mass):
+    print(f"\nEarth mass in pounds is: {mass}")
 
-    planet_index = planets.index(planet)
-    print(f"Your weight in {planets[planet_index]} is {(w_kg * g_ms2[planet_index]) / n_lb} lb. ")
+    # Convert your mass into kilograms.
+    m_kg = mass / 2.2046
+    print(f"Mass in kg: {round(m_kg , 2)}")
 
-calculateWeight(myPlanet, myWeight)
+    # Convert the planet input string into a index integer.
+    planet = planets.index(planet)
+
+    # Weight in Newtons: Calculate weight by multiplying mass and gravitational acceleration.
+    w_n = m_kg * g_ms2[planet]
+
+    # Newtons in a pound constant.
+    n_lb = 4.45 
+
+    # Calculate the weight inside of a functional string.
+    print(f"Weight in {planets[planet]} is {round( w_n / n_lb,2)} lb. \n")
+ 
+calculateWeight(myPlanet,myMass)
