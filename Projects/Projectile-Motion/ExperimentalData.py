1747057882b3fff1bc38eac1469f3a2dacafabb9
CSI-Mario-Sanchez <22-076@sanignacio.pr>
2021-10-15 11:56:04 -0400

    CSI-Mario-Sanchez
    projectile class

--- Projects/Projectile-Motion/ExperimentalData.py
+++ Projects/Projectile-Motion/ExperimentalData.py
@@ -8,18 +8,20 @@ class ExperimentalData:
     # buildingName = "Soleil"
     # height = 88
 
-    def __init__(self,weapon:str, projectile:str, velocity:float, building:str, altitude:float):
+    def __init__(self,weapon:str, projectile:str, ammunition:str, velocity:float, building:str, altitude:float, gravity:float): # sets up the variables
         self.weaponName = weapon
         self.projectileName = projectile
+        self.ammunitionType = ammunition
         self.speed = velocity
         self.buildingName = building
         self.height = altitude
+        self.gravity = gravity
 
 
     def buildingFireTime(self, height): #Takes height of the building
         time = math.sqrt(height/(9.8/2)) # Uses gravitational acceleration to find the time given a distance
-        return time
+        return time # gives the time value
 
     def buildingFireDistance(self, time, velocity): #Takes the time to hit the ground and velocity in x axis
         distance = time * velocity # Uses the time and velocity to find distance
-        return distance
+        return distance # gives the distance value
--- Projects/Projectile-Motion/Projectile-Motion.py
+++ Projects/Projectile-Motion/Projectile-Motion.py
@@ -1,14 +1,12 @@
 import math
 import json
-import ExperimentalData
+from ExperimentalData import ExperimentalData
+
+def experiment(experimentData:ExperimentalData):
+    time = experimentData.buildingFireTime(experimentData.height) #
+    distance = experimentData.buildingFireDistance(time,experimentData.speed) # Uses the time from building fire time and speed to find the distance
+    print(f"The shot was fired from a {experimentData.weaponName} gun using a {experimentData.projectileName} {experimentData.ammunitionType} bullet. \nThe shot has an initial velocity of {experimentData.speed}. \nThe shot was fired from the {experimentData.buildingName} building, with a height of {experimentData.height} meters. \nThe shot fired for {round(distance)} meters and took {round(time)} seconds to reach the ground with a gravity of {experimentData.gravity} m/s^2.")
+
 
 if __name__ == "__main__":
-    experimentalData = ExperimentalData("AK-105", "5.45", 905, "Soleil", 88)
-    time = experimentalData.buildingFireTime(experimentalData.height)
-    distance = experimentalData.buildingFireDistance(time,experimentalData.speed) # Uses the time from building fire time and speed to find the distance
-    print(f"The shot was fired from a {experimentalData.weaponName} gun using a {experimentalData.projectileName} bullet. \nThe shot has an initial velocity of {experimentalData.speed}. \nThe shot was fired from the {experimentalData.buildingName} building, with a height of {experimentalData.height} meters. \nThe shot fired for {round(distance)} meters and took {round(time)} seconds to reach the ground.")
-    jsonstring = json.dumps(experimentalData.__dict__)
-    print(jsonstring)
-
-    def experiment(experimentData:ExperimentalData):
-        print()
+    experiment(ExperimentalData("AK-105", "5.45", "PS gs", 905, "Soleil", 88, 9.8))
