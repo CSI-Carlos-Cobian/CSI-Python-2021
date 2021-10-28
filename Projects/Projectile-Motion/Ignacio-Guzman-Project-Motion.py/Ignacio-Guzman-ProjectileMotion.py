f42de2ba72aae8f42b60d8480f4059b93b6a9fc2
CSI-Ignacio-Guzman <89158849+CSI-Ignacio-Guzman@users.noreply.github.com>
2021-10-28 14:33:59 -0400

    ma

---
+++ Projects/Projectile-Motion/Ignacio-Guzman-Project-Motion.py/Ignacio-Guzman-ProjectileMotion.py
@@ -0,0 +1,20 @@
+import math
+gun = "GLOCK17"
+caliber = "9x19mm"
+ammunition = "PBB gzh"
+velocity_ms= 560
+
+Building = "Caribean Sea View"
+BuildingHeight = 334
+
+
+gravity_Ms = 9.81
+
+
+
+time_s = math.sqrt ((2 * 334) /  9.81)
+distance = (30 * 68)
+
+print(f"The gun selected was "GLOCK17")
+
+"Caribean Sea View"
