c15bd7b6e59e9813d29d90e74767a53aaf5ada70
CSI-Daniel-Cintron <89158899+CSI-Daniel-Cintron@users.noreply.github.com>
2021-11-03 14:52:48 -0400

    Update Project

--- Projects/Projectile-Motion/Daniel-Cintron-ProjectileMotion.py
+++ Projects/Projectile-Motion/Daniel-Cintron-ProjectileMotion.py
@@ -1,5 +1,5 @@
 import math
-
+from ExperimentalData import ExperimentalData
 # gun = "DT MDR"
 # cartridge = "5.56x45mm"
 # rounds = "HP" 
@@ -11,12 +11,29 @@ import math
 # gravity = 9.81
 
 
-def ProjectileFunction(gun:str, cartridge:str, rounds:str, velocity_ms:int, Building:str, Buildingsweight:str, gravity:str)
-time_s = math.sqrt((2 * BuildingHeight) / gravity)
+def ProjectileFunction(experimentalData:ExperimentalData):
+    
+    time_s =math.sqrt((2 * BuidingHeight) / gravity)
 
-distance = (velocity_ms * time_s)
+    # distance = experimentationData([velocity_ms] * time_s)
+    distance = (velocity_ms * time_s) 
 
-print(f"The gun chosen was {gun} its cartridge is {cartridge}, its rounds are {rounds} and the bullets velocity was {velocity_ms}.m/s")
-print("Its the product of the US-based company Desert Tech LLC MDR rifle is a modular, multi-caliber weapon with compact bullpup layout. Barrel lengths and calibers can be changed by the end user within minutes with a minimum amount of tools.")
+    print(f"The gun chosen was {gun} its cartridge is {cartridge}, its rounds are {rounds} and the bullets velocity was {velocity_ms}.m/s")
+    print("Its the product of the US-based company Desert Tech LLC MDR rifle is a modular, multi-caliber weapon with compact bullpup layout. Barrel lengths and calibers can be changed by the end user within minutes with a minimum amount of tools.")
 
-ProjectileFunction("DT MDR", "5.56x45mm", "HP", 941, "Minillas South Tower", 244, 9.81)
+#Convert your parameters into a single JSON Object.
+# experimentationData = {
+
+#  "gun" : "DT MDR",
+#  "cartridge" : "5.56x45mm",
+#  "rounds" : "HP", 
+#  "velocity_ms" : 941,
+#  "Building" : "Minillas South Tower",
+#  "BuildingHeight" : 244,
+#  "gravity" : 9.81
+
+# }
+
+
+experimentalData = ExperimentalData("DT MDR", "5.56x45mm", "HP", 941, "Minillas South Tower", 244, 9.81)
+ProjectileFunction(experimentalData)
---
+++ Projects/Projectile-Motion/ExperimentalData.py
@@ -0,0 +1,9 @@
+class ExperimentalData:
+    def __init__(self, gun:str, cartridge:str, rounds:str, velocity_ms:int, Building:str, BuildingHeight:str, gravity:int):
+        self.gun = gun
+        self.cartridge = cartridge
+        self.rounds = rounds
+        self.velocity_ms = velocity_ms
+        self.Building = Building
+        self.BuildingHeight = BuildingHeight
+        self.gravity = gravity
