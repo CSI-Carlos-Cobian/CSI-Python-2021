0c40998ec6624e2fa0a16db61d63028d3679add2
CSI-Daniel-Cintron <89158899+CSI-Daniel-Cintron@users.noreply.github.com>
2021-11-11 15:06:15 -0400

    Planets update

--- Projects/Projectile-Motion/Daniel-Cintron-ProjectileMotion.py
+++ Projects/Projectile-Motion/Daniel-Cintron-ProjectileMotion.py
@@ -17,15 +17,17 @@ import os
 
 
 def ProjectileFunction(experimentalData:ExperimentalData):
-    
-    time_s =math.sqrt((2 * experimentalData.BuildingHeight) / experimentalData.gravity)
+    planets = ["Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune"]
+    g_ms2 = [3.7, 8.87, 9.81, 3.711, 24.79, 10.44, 8.69, 11.15]
+
+    time_s =math.sqrt((2 * experimentalData.BuildingHeight) / experimentalData.planets)
 
     # distance = experimentationData([velocity_ms] * time_s)
     distance = (experimentalData.velocity_ms * time_s) 
 
     print(f"The gun chosen was {experimentalData.gun} its cartridge is {experimentalData.cartridge}, its rounds are {experimentalData.rounds} and the bullets velocity was {experimentalData.velocity_ms}.m/s")
     print("Its the product of the US-based company Desert Tech LLC MDR rifle is a modular, multi-caliber weapon with compact bullpup layout. Barrel lengths and calibers can be changed by the end user within minutes with a minimum amount of tools.")
-    print(f"The projectile will go to a distance of {distance}, and the time it will take to cover it is {time_s} ")
+    print(f"The projectile will go to a distance of {distance}, and the time it will take to cover it is {time_s} and the planet is {experimentalData.planets} ")
 #Convert your parameters into a single JSON Object.
 # experimentationData = {
 
@@ -35,19 +37,19 @@ def ProjectileFunction(experimentalData:ExperimentalData):
 #  "velocity_ms" : 947,
 #  "Building" : "Minillas South Tower",
 #  "BuildingHeight" : 244,
-#  "gravity" : 9.81
+#  "Planet" : 9.81
 
 # }
 
 
-experimentalData = ExperimentalData("DT MDR", "5.56x45mm", "HP", 941, "Minillas South Tower", 244, 9.81)
+#experimentalData = ExperimentalData("DT MDR", "5.56x45mm", "HP", 941, "Minillas South Tower", 244, 9.81)
 
 myDataSet = [
-ExperimentalData("DT MDR", "5.56x45mm", "HP", 947, "Minillas South Tower", 244, 9.81),
-ExperimentalData("DT MDR", "5.56x45mm", "FMJ", 957, "Minillas South Tower", 244, 9.81),
-ExperimentalData("DT MDR", "5.56x45mm", "M855", 922, "Minillas South Tower", 244, 9.81), 
-ExperimentalData("DT MDR", "5.56x45mm", "M855A1", 945, "Minillas South Tower", 244, 9.81),   
-ExperimentalData("DT MDR", "5.56x45mm", "M856", 874, "Minillas South Tower", 244, 9.81)
+ExperimentalData("DT MDR", "5.56x45mm", "HP", 947, "Minillas South Tower", 244, "Earth"),
+ExperimentalData("DT MDR", "5.56x45mm", "FMJ", 957, "Minillas South Tower", 244, "Mars"),
+ExperimentalData("DT MDR", "5.56x45mm", "M855", 922, "Minillas South Tower", 244, "Jupiter"), 
+ExperimentalData("DT MDR", "5.56x45mm", "M855A1", 945, "Minillas South Tower", 244, "Neptune"),   
+ExperimentalData("DT MDR", "5.56x45mm", "M856", 874, "Minillas South Tower", 244, "Saturn")
 ]
 
 for data in myDataSet:
@@ -58,8 +60,17 @@ for data in myDataSet:
 myOutputPath = Path(__file__).parents[0]
 myOutputFilePath = os.path.join(myOutputPath, 'Projectile-Motion.json')
 
+print(myOutputFilePath)
+
 with open(myOutputFilePath, 'w') as outfile:
     json.dump([data.__dict__ for data in myDataSet], outfile)
+
+    #Deserialzation
+    deserialize = open(myOutputFilePath)
+    experimentJson = json.load(deserialize)
+
+    for e in experimentJson:
+        ExperimentalData(**e).run()
     #json.dump(myDataSet[0].__dict__, outfile)
 
 # ProjectileFunction(experimentalData)
--- Projects/Projectile-Motion/ExperimentalData.py
+++ Projects/Projectile-Motion/ExperimentalData.py
@@ -1,9 +1,9 @@
 class ExperimentalData:
-    def __init__(self, gun:str, cartridge:str, rounds:str, velocity_ms:int, Building:str, BuildingHeight:str, gravity:int):
+    def __init__(self, gun:str, cartridge:str, rounds:str, velocity_ms:int, Building:str, BuildingHeight:str, planets:str):
         self.gun = gun
         self.cartridge = cartridge
         self.rounds = rounds
         self.velocity_ms = velocity_ms
         self.Building = Building
         self.BuildingHeight = BuildingHeight
-        self.gravity = gravity
+        self.planets = planets
