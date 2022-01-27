c66faf3ca2e65b8173599b13206f857fcd02b7b3
CSI-Mario-Sanchez <22-076@sanignacio.pr>
2022-01-27 11:30:48 -0400

    CSI-Mario-Sanchez
    Random Lorem Ipsum Class + JSON

---
+++ Projects/Web-Servers/Lorem_Ipsum.py
@@ -0,0 +1,14 @@
+class Lorem_Ipsum:
+    def __init__(self, id:int, uid:str, word:str, words, characters:str, short_sentence:str, long_sentence:str, very_long_sentence:str, paragraphs, question:str, questions):
+        self.id = id
+        self.uid = uid
+        self.word = word
+        self.words = words
+        self.characters = characters
+        self.short_sentence = short_sentence
+        self.long_sentence = long_sentence
+        self.very_long_sentence = very_long_sentence
+        self.paragraphs = paragraphs
+        self.question = question
+        self.questions = questions
+
