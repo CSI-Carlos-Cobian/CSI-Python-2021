45d52d5589f8794a6bd5a1e511b1ba923d8d6f46
CSI-Mario-Sanchez <22-076@sanignacio.pr>
2022-02-02 11:45:17 -0400

    CSI-Mario-Sanchez
    File Request finished + git ignore

--- Projects/Web-Servers/FileRequest.py
+++ Projects/Web-Servers/FileRequest.py
@@ -6,8 +6,12 @@ import os
 
 ssl._create_default_https_context = ssl._create_unverified_context
 
-ipsumURL = "https://random-data-api.com/api/lorem_ipsum/random_lorem_ipsum"
+ipsumURL = "https://random-data-api.com/api/lorem_ipsum/random_lorem_ipsum?size=100"
 ipsumList:Lorem_Ipsum = []
+
+myPath = Path(__file__).parents[0] # gets the path for the json
+myFolderPath = os.path.join(myPath, "requests")
+os.mkdir(myFolderPath)
 
 for i in range(100):
     req = urllib.request.Request(ipsumURL)
@@ -16,10 +20,10 @@ for i in range(100):
     for r in serializedData:
         newIpsum:Lorem_Ipsum = Lorem_Ipsum(**r)
         ipsumList.append(newIpsum)
-        print(newIpsum.id)
-        
-        myOutputPath = Path(__file__).parents[0] # gets the path for the json
-        myOutputFilePath = os.path.join(myOutputPath, f'request_{i}.json')
-        with open(myOutputFilePath, 'w') as outfile:
-            json.dump([ipsum.__dict__ for ipsum in ipsumList], outfile)
-            
+        print(newIpsum.uid)
+    
+        newFilePath = os.path.join(myFolderPath, f"request{newIpsum.uid}.json")
+
+        with open(newFilePath, 'w') as outfile:
+            json.dump(newIpsum.__dict__, outfile)
+
