aa3f6d54c7cfd0ee082d594512e393ef8be58e03
CSI-Carlos-Cobian <carlos.cobian@sanignacio.pr>
2022-02-10 11:01:51 -0400

    Hangman and Web-Servers Update

---
+++ Projects/Web-Servers/Entities.py
@@ -0,0 +1,25 @@
+class Address:
+  def __init__(self,id:int,uid:str,city:str,street_name:str,street_address:str,secondary_address:str,building_number:str,mail_box:str,community:str,zip_code:str,zip:str,postcode:str,time_zone:str,street_suffix:str,city_suffix:str,city_prefix:str,state:str,state_abbr:str,country:str,country_code:str,latitude:str,longitude:str,full_address:str):
+    self.id = id
+    self.uid = uid
+    self.city = city
+    self.street_name = street_name
+    self.street_address = street_address
+    self.secondary_address = secondary_address
+    self.building_number = building_number
+    self.mail_box = mail_box
+    self.community = community
+    self.zip_code = zip_code
+    self.zip = zip
+    self.postcode = postcode
+    self.time_zone = time_zone
+    self.street_suffix = street_suffix
+    self.city_suffix = city_suffix
+    self.city_prefix = city_prefix
+    self.state = state
+    self.state_abbr = state_abbr
+    self.country = country
+    self.country_code = country_code
+    self.latitude = latitude
+    self.longitude = longitude
+    self.full_address = full_address
