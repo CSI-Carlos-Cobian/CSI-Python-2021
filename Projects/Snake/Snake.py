0a6ffd72c87d8fecd046216180594b0b8293c323
CSI-Mario-Sanchez <22-076@sanignacio.pr>
2022-03-09 11:28:25 -0400

    CSI-Mario-Sanchez
    First Snake commit: block of code for display + comments

---
+++ Projects/Snake/Snake.py
@@ -0,0 +1,7 @@
+import pygame
+
+pygame.init() # initializes the pygame functions
+dis=pygame.display.set_mode((400,300)) # sets the size of the display
+pygame.display.update() #updates the size of the display
+pygame.quit() # quits the game
+quit() # ends the execution of the code
