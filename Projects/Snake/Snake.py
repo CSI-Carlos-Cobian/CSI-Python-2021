db90e9be6647a4dbbfd9278f4298296deea82659
CSI-Mario-Sanchez <22-076@sanignacio.pr>
2022-03-09 11:40:02 -0400

    CSI-Mario-Sanchez
    Snake Game third commit: removes printing, adds end condition + comments

--- Projects/Snake/Snake.py
+++ Projects/Snake/Snake.py
@@ -7,6 +7,7 @@ pygame.display.set_caption("Snake Game") # sets the name of the window
 game_over = False # boolean to decide whether the game will be ongoing or not
 while not game_over: # while the game is running
     for event in pygame.event.get(): # runs through every possible event
-        print(event) # prints every event
+        if event.type == pygame.QUIT: # if the event is quitting
+            game_over = True # ends the game
 pygame.quit() # quits the game
 quit() # ends the execution of the code
