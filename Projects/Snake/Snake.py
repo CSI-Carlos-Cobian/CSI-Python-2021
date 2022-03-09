2edbe5201fbef511ebf4774fa88466ef4f335119
CSI-Mario-Sanchez <22-076@sanignacio.pr>
2022-03-09 11:38:02 -0400

    CSI-Mario-Sanchez
    Snake Game second commit: prints the events occurring, adds game over without lose state + comments

--- Projects/Snake/Snake.py
+++ Projects/Snake/Snake.py
@@ -3,5 +3,10 @@ import pygame
 pygame.init() # initializes the pygame functions
 dis=pygame.display.set_mode((400,300)) # sets the size of the display
 pygame.display.update() #updates the size of the display
+pygame.display.set_caption("Snake Game") # sets the name of the window
+game_over = False # boolean to decide whether the game will be ongoing or not
+while not game_over: # while the game is running
+    for event in pygame.event.get(): # runs through every possible event
+        print(event) # prints every event
 pygame.quit() # quits the game
 quit() # ends the execution of the code
