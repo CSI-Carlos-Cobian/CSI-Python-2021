adcc6d66a2a7a71a5c94e4476fbb8b52547d927a
CSI-Ignacio-Guzman <89158849+CSI-Ignacio-Guzman@users.noreply.github.com>
2022-04-08 14:29:02 -0400

    msms

--- Projects/Web-Scraping/webscrapping02 Ignacio-Guzman02.py
+++ Projects/Web-Scraping/webscrapping02 Ignacio-Guzman02.py
@@ -2,4 +2,6 @@ import requests
 res = requests.get(https://www.gutenberg.org/ebooks/67637)
 res.raise_for_status()
 playfile = open(The History of the Manners and Customs of Ancient Greece Volume II (of 3))
-for 
+for chunk in res.iter_content(10000):
+    playFile.write(chunk)
+playFile.close()
